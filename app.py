dd9f05730d7733fccf478640479b8d44a2e54915
Muhammad Ali <aliranaa708@gmail.com>
2025-11-07 11:21:53 +0500

    Update home route return message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Do it  from the folder Flask inside Docker!"
+    return "Returnit that from the folder Flask inside Docker!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
